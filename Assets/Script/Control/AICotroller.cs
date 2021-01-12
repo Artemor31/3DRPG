8c9d5130ff18d3bed7fbae47146f72067bb75abf
Artyom <artem.artemov311@gmail.com>
2021-01-12 20:42:16 +0300

    Added some Ai states

--- Assets/Script/Control/AICotroller.cs
+++ Assets/Script/Control/AICotroller.cs
@@ -11,13 +11,14 @@ namespace RPG.Control
     public class AICotroller : MonoBehaviour
     {
         [SerializeField] float _chaseDistance = 5f;
-
+        [SerializeField] float _suspiciousTime = 5f;
 
         GameObject _player;
         Fighter _fighter;
         Health _health;
         Mover _mover;
         Vector3 _startPosition;
+        float _timeSincelastPlayerSaw = Mathf.Infinity;
 
 
         private void Start()
@@ -30,22 +31,50 @@ namespace RPG.Control
         }
         private void Update()
         {
-            if (_health.Dead()) return;
+
+            if (_health.Dead())
+            {
+                return;
+            }
             if (DistanceToPlayer() && _fighter.CanAttack(_player))
             {
-                _fighter.Attack(_player);
+                _timeSincelastPlayerSaw = 0;
+                AggroBehaviour();
+            }
+            else if(_timeSincelastPlayerSaw < _suspiciousTime)
+            {
+                SuspiciousBehaviour();
             }
             else
             {
-                _mover.StartMove(_startPosition);
+                GuardBehaviour();
             }
+
+
+            _timeSincelastPlayerSaw += Time.deltaTime;
         }
+
+        private void GuardBehaviour()
+        {
+            _mover.StartMove(_startPosition);
+        }
+
+        private void AggroBehaviour()
+        {
+            _fighter.Attack(_player);
+        }
+
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = Color.Lerp(Color.red, Color.green, 0.3f);
             Gizmos.DrawWireSphere(transform.position, _chaseDistance);
         }
 
+        void SuspiciousBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+
+        }
         private bool DistanceToPlayer()
         {
             return Vector3.Distance(_player.transform.position, transform.position) < _chaseDistance;
